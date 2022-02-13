288c6b49384099b753186cd47dafdce00908219d
Miguel Figuera <miguelqui725@gmail.com>
2022-02-13 17:26:17 -0400

    asembling the looks...

---
+++ script.js
@@ -0,0 +1,7 @@
+//header
+let divHeader=document.getElementsByClassName("header");
+let h1=document.createElement("h1");
+divHeader[0].appendChild(h1);
+h1.innerText= " Pixel Sketching";
+h1.style.textAlign = "center"
+h1.style.color= "white";
