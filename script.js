657e3a4b06e1c560616301d479401b1c7dbaaa48
Miguel Figuera <miguelqui725@gmail.com>
2022-02-28 17:25:02 -0400

    working on JS

---
+++ script.js
@@ -0,0 +1,25 @@
+
+//button const
+const colorMode = document.getElementById("colorMode");
+const rainbowMode= document.getElementById("rainbowMode");
+const eraser = document.getElementById("eraser");
+const clearGrid= document.getElementById("clearGrid");
+const colorGrabber= document.getElementById("colorGrabber");
+const shading= document.getElementById("shading");
+const lighten= document.getElementById("lighten");
+const viewGridLines = document.getElementById("gridLines");
+const sizeSlider=document.getElementById("sizeSelection");
+let sizeNumber = document.getElementById ("size");
+
+//Slider function
+sizeNumber.innerText = sizeSlider.value + " x " + sizeSlider.value;
+sizeSlider.onchange = () => {sizeNumber.innerText = sizeSlider.value + " x " + sizeSlider.value; createGrid()}
+
+//grid creation
+const grid=document.getElementById("gridDiv");
+function createGrid() {
+    grid.style.gridTemplateColumns = 'repeat(${sizeSlider.value}, 1fr)';
+    grid.style.gridTemplateRows= 'repeat(${sizeSlider.value}, 1fr)'; 
+
+    for(let i=0; i < sizeSlider.value * sizeSlider.value; i++)
+}
